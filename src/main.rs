bcbce90e4f473ac715c3d15fb1b3b3cf9e553b93
mc_fdc <Masato.04.11.2007@gmail.com>
2022-11-01 01:49:32 +0000

    push

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-use axum
+use axum::{
+    routing::get,
+    Router,
+    extract::Extension
+};
+
+use std::result::Result;
+use sqlx::mysql::MySqlPool;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error>{
+    let pool = MySqlPool::connect("")
+        .await?;
+    let app = Router::new()
+        .route("/", get(|| async {"hello"}))
+        .layer(Extension(pool));
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    Ok(())
+}
